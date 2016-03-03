8c884069e3591f4536c2436b86212532d5cb4414
Julien Palard <julien@palard.fr>
2016-03-03 09:52:22 +0100

    Enhance setup.py (and add licence)

--- setup.py
+++ setup.py
@@ -8,16 +8,23 @@ from distutils.core import setup, Extension
 
 
 hl_vt100_module = Extension('_hl_vt100',
-                           sources=['hl_vt100_wrap.c',
-                                    'src/hl_vt100.c',
-                                    'src/lw_terminal_parser.c',
-                                    'src/lw_terminal_vt100.c'],
-                           )
+                            sources=['hl_vt100_wrap.c',
+                                     'src/hl_vt100.c',
+                                     'src/lw_terminal_parser.c',
+                                     'src/lw_terminal_vt100.c'])
 
-setup (name = 'hl_vt100',
-       version = '0.1',
-       author      = "Julien Palard",
-       description = """Headless vt100 emulator""",
-       ext_modules = [hl_vt100_module],
-       py_modules = ["hl_vt100"],
-       )
+setup(name='hl_vt100',
+      version='0.1',
+      author="Julien Palard",
+      description="""Headless vt100 emulator""",
+      ext_modules=[hl_vt100_module],
+      py_modules=["hl_vt100"],
+      classifiers=[
+          "Programming Language :: C",
+          "Programming Language :: Python",
+          "Development Status :: 5 - Production/Stable",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: OS Independent",
+          "Topic :: System :: Emulators",
+          "Topic :: Terminals :: Terminal Emulators/X Terminals"
+      ])
